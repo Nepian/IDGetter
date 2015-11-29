a3552382c4ab73a0b184da14b615a54e949decd2
Nepian <nepia.ms@gmail.com>
2015-11-30 07:56:01 +0900

    Plugin に PlayerInteract Listener を登録

--- src/main/java/com/Nepian/IDGetter/IDGetter.java
+++ src/main/java/com/Nepian/IDGetter/IDGetter.java
@@ -1,7 +1,18 @@
 package com.Nepian.IDGetter;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.Nepian.Listener.PlayerInteract;
 
 public class IDGetter extends JavaPlugin {
 
+	@Override
+	public void onEnable() {
+		this.registerEvent(new PlayerInteract());
+	}
+
+	private void registerEvent(Listener listener) {
+		this.getServer().getPluginManager().registerEvents(listener, this);
+	}
 }
