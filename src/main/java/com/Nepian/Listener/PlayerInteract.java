a00a1d280ff19b911727705934c746c7cbc658bd
Nepian <nepia.ms@gmail.com>
2015-11-30 11:32:35 +0900

    PlayerInteract Listener メソッド追加
    プレイヤーが右クリックした際に、ブロックをクリックしておらず、アイテムを手に持っていた場合に、そのアイテムのIDとData、Material名を表示するメソッドを追加

--- src/main/java/com/Nepian/Listener/PlayerInteract.java
+++ src/main/java/com/Nepian/Listener/PlayerInteract.java
@@ -1,17 +1,19 @@
 package com.Nepian.Listener;
 
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class PlayerInteract implements Listener {
 
 	@SuppressWarnings("deprecation")
 	@EventHandler
-	public static void onPlayerInteract(PlayerInteractEvent event) {
+	public static void onPlayerInteractBlockRightClick(PlayerInteractEvent event) {
 
 		if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
 			return;
@@ -30,6 +32,34 @@ public class PlayerInteract implements Listener {
 		msg.append(blockData).append(", ");
 		msg.append(blockType);
 
+		player.sendMessage(msg.toString());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public static void onPlayerInteractItemInHand(PlayerInteractEvent event) {
+		
+		if (event.getAction() != Action.RIGHT_CLICK_AIR) {
+			return;
+		}
+		
+		Player player = event.getPlayer();
+		ItemStack item = player.getItemInHand();
+		
+		if (item.getType() == Material.AIR) {
+			return;
+		}
+		
+		String itemType = item.getType().toString();
+		int itemTypeId = item.getTypeId();
+		byte itemData = item.getData().getData();
+		
+		StringBuilder msg = new StringBuilder("");
+		
+		msg.append("ID: ").append(itemTypeId).append(":");
+		msg.append(itemData).append(", ");
+		msg.append(itemType);
+		
 		player.sendMessage(msg.toString());
 	}
 }
